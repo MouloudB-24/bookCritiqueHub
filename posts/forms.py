573d34170784d234efc9631e2d9cfda696c63a75
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2024-10-22 11:32:36 +0200

    Add style to forms

--- posts/forms.py
+++ posts/forms.py
@@ -7,9 +7,19 @@ class TicketForm(forms.ModelForm):
     class Meta:
         model = models.Ticket
         fields = ['title', 'description', 'image']
+        labels = {
+            'title': 'Titre',
+            'description': 'Description',
+            'image': 'Image',
+        }
 
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = models.Review
         fields = ['headline', 'rating', 'body']
+        labels = {
+            'headline': 'Titre de la critique',
+            'rating': 'Note',
+            'body': 'Contenu',
+        }
