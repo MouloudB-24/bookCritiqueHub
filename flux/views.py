5b31de6ae3c8dffeb302e9639b9cafefb1d8c432
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2024-10-23 09:23:53 +0200

    Add subscriptions style

--- flux/views.py
+++ flux/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import View
 
 from posts.models import Ticket, Review
+from subscriptions.models import UserFollows
 
 
 class HomePage(LoginRequiredMixin, View):
@@ -10,6 +11,11 @@ class HomePage(LoginRequiredMixin, View):
     login_url = 'login'
 
     def get(self, request):
-        tickets = Ticket.objects.all()
-        reviews = Review.objects.all()
+
+        """following = UserFollows.objects.filter(user=request.user).value_list('followed_user', flat=True)
+        followers = UserFollows.objects.filter(followed_user=request.user).value_list('user', flat=True)
+        users = list(following) + list(followers) + [request.user.id]"""
+
+        tickets = Ticket.objects.filter()
+        reviews = Review.objects.filter()
         return render(request, self.template_name, context={'tickets': tickets, 'reviews': reviews})
